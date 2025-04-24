6d217e954c35c7152032dc70d5714e35c142022c
Steve Ho <jobs.steve54@gmail.com>
2025-04-24 09:35:10 +0800

    fix: 修正從 AuthorProfile 元件的引入方式，改為使用命名匯出，提升代碼一致性與可讀性。

--- src/pages/author-profile/index.tsx
+++ src/pages/author-profile/index.tsx
@@ -2,7 +2,7 @@ import { NextPage } from 'next';
 
 import { Header } from '@/components/layout/Header';
 import { Footer } from '@/components/layout/Footer';
-import AuthorProfile from '@/components/AuthorProfile';
+import { AuthorProfile } from '@/components/AuthorProfile';
 import { mockAuthor, mockRecipes } from '@/components/AuthorProfile/types';
 
 const AuthorProfilePage: NextPage = () => {
