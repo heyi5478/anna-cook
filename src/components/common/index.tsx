27c7122017b5379a549cf95e548aaf202bcdd6fd
Steve Ho <jobs.steve54@gmail.com>
2025-07-22 12:08:31 +0800

    refactor: 簡化 common 元件匯出，移除不必要的命名衝突

--- src/components/common/index.tsx
+++ src/components/common/index.tsx
@@ -1,7 +1,4 @@
+export { default as RotationPrompt } from './RotationPrompt';
 export { default as StepIndicator } from './StepIndicator';
-export { VimeoPlayer } from './VimeoPlayer';
 export { UserProfileCard } from './UserProfileCard';
-export {
-  RotationPrompt,
-  default as DefaultRotationPrompt,
-} from './RotationPrompt';
+export { VimeoPlayer } from './VimeoPlayer';
